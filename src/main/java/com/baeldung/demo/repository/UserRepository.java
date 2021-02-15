674e96b69c81d3176a096049a36aa327026c47b7
musadabra <musadabra@gmail.com>
2021-02-15 09:48:57 +0100

    Create User repository

---
+++ src/main/java/com/baeldung/demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.baeldung.demo.repository;
+
+import com.baeldung.demo.model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    public User getUserByUsername(@Param("username") String username);
+}
