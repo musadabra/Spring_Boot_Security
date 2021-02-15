b072f6c5cb4809c722b896bbaf99b1bfb74d802e
musadabra <musadabra@gmail.com>
2021-02-15 09:38:02 +0100

    Updated db.sql

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(45) NOT NULL,
-  `full_name` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `full_name` varchar(45),
   `password` varchar(64) NOT NULL,
   `enabled` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -9,7 +10,7 @@ CREATE TABLE `users` (
 );
 
 CREATE TABLE `roles` (
-  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 );
@@ -19,8 +20,8 @@ CREATE TABLE `users_roles` (
   `role_id` int(11) NOT NULL,
   KEY `user_fk_idx` (`user_id`),
   KEY `role_fk_idx` (`role_id`),
-  CONSTRAINT `role_fk` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`),
-  CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+  CONSTRAINT `role_fk` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
 
 INSERT INTO `roles` (`name`) VALUES ('USER');
@@ -28,11 +29,11 @@ INSERT INTO `roles` (`name`) VALUES ('CREATOR');
 INSERT INTO `roles` (`name`) VALUES ('EDITOR');
 INSERT INTO `roles` (`name`) VALUES ('ADMIN');
 
-INSERT INTO `users` (`username`, `password`, `enabled`) VALUES ('musa', '$2a$10$cTUErxQqYVyU2qmQGIktpup5chLEdhD2zpzNEyYqmxrHHJbSNDOG.', '1');
-INSERT INTO `users` (`username`, `password`, `enabled`) VALUES ('hassan', '$2a$10$.tP2OH3dEG0zms7vek4ated5AiQ.EGkncii0OpCcGq4bckS9NOULu', '1');
-INSERT INTO `users` (`username`, `password`, `enabled`) VALUES ('dabra', '$2a$10$E2UPv7arXmp3q0LzVzCBNeb4B4AtbTAGjkefVDnSztOwE7Gix6kea', '1');
-INSERT INTO `users` (`username`, `password`, `enabled`) VALUES ('namhm', '$2a$10$GQT8bfLMaLYwlyUysnGwDu6HMB5G.tin5MKT/uduv2Nez0.DmhnOq', '1');
-INSERT INTO `users` (`username`, `password`, `enabled`) VALUES ('admin', '$2a$10$IqTJTjn39IU5.7sSCDQxzu3xug6z/LPU6IF0azE/8CkHCwYEnwBX.', '1');
+INSERT INTO `users` (`email`, `username`, `password`, `enabled`) VALUES ('musa@gmail.com', 'musa', '$2a$10$cTUErxQqYVyU2qmQGIktpup5chLEdhD2zpzNEyYqmxrHHJbSNDOG.', '1');
+INSERT INTO `users` (`email`, `username`, `password`, `enabled`) VALUES ('hassan@gmail.com', 'hassan', '$2a$10$.tP2OH3dEG0zms7vek4ated5AiQ.EGkncii0OpCcGq4bckS9NOULu', '1');
+INSERT INTO `users` (`email`, `username`, `password`, `enabled`) VALUES ('dabra@gmail.com', 'dabra', '$2a$10$E2UPv7arXmp3q0LzVzCBNeb4B4AtbTAGjkefVDnSztOwE7Gix6kea', '1');
+INSERT INTO `users` (`email`, `username`, `password`, `enabled`) VALUES ('namhm@gmail.com', 'namhm', '$2a$10$GQT8bfLMaLYwlyUysnGwDu6HMB5G.tin5MKT/uduv2Nez0.DmhnOq', '1');
+INSERT INTO `users` (`email`, `username`, `password`, `enabled`) VALUES ('admin@gmail.com', 'admin', '$2a$10$IqTJTjn39IU5.7sSCDQxzu3xug6z/LPU6IF0azE/8CkHCwYEnwBX.', '1');
 
 INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (1, 1); -- user musa has role USER
 INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (2, 2); -- user hassan has role CREATOR
